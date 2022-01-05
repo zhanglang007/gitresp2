e858fefb2dfbacf727a2fa4aaea6af24b4d79527
zhanglang <907198516@qq.com>
2022-01-06 00:20:19 +0800

    新增stringbulider

--- src/com/exec/striver.java
+++ src/com/exec/striver.java
@@ -2,6 +2,7 @@ package com.exec;
 
 public class striver {
     private int a;
+    private  StringBuilder stringBuilder;
     public static void main(String[] args) {
         System.out.println("sjioghjsi");
     }
